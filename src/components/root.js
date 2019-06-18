1b9cf2209012982cefbc2ddf737295dc83291e17
Lars Levie <larslevie@gmail.com>
2019-06-18 08:36:58 -0500

    Install emotion

--- src/components/root.js
+++ src/components/root.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { css } from '@emotion/core';
 
-const cell = title => <div>{title}</div>;
+const gridClass = css`
+  min-height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+`;
 
-const Root = () => <div>{cell('Monday')}</div>;
+const cellClass = css`
+  display: flex;
+  flex-basis: 50%;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const renderCell = title => (
+  <div key={title} css={cellClass}>
+    {title}
+  </div>
+);
+
+const renderCells = () =>
+  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Next'].map(day =>
+    renderCell(day));
+
+const Root = () => <div css={gridClass}>{renderCells()}</div>;
 
 export default Root;
