a98d26225892637b4236f1084d4828aff1251af4
Lars Levie <larslevie@gmail.com>
2019-07-04 06:09:50 -0500

    Just edit whenever you want

--- src/components/task-list/task.js
+++ src/components/task-list/task.js
@@ -1,62 +1,41 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React from 'react';
 
 const TaskForm = ({ render }) => render();
 
-const Task = ({ task, dispatch }) => {
-  const [editMode, setEditMode] = useState(false);
+const renderTask = (dispatch, task) => (
+  <div>
+    <input
+      type="checkbox"
+      checked={task.isComplete}
+      onChange={() => dispatch({ ...task, type: 'complete' })}
+    />
+    <input
+      type="checkbox"
+      checked={task.isDeferred}
+      onChange={() => dispatch({ ...task, type: 'defer' })}
+    />
+    <input
+      type="checkbox"
+      checked={task.isCanceled}
+      onChange={() => dispatch({ ...task, type: 'cancel' })}
+    />
+    <input
+      type="text"
+      name="label"
+      value={task.label}
+      onChange={({ target: { value } }) => {
+        dispatch({
+          ...task,
+          type: 'update',
+          label: value,
+        });
+      }}
+    />
+  </div>
+);
 
-  return (
-    <div>
-      {editMode ? (
-        <TaskForm
-          task={task}
-          render={() => (
-            <div>
-              <input
-                type="text"
-                name="label"
-                value={task.label}
-                onChange={({ target: { value } }) => {
-                  dispatch({
-                    ...task,
-                    type: 'update',
-                    label: value,
-                  });
-                }}
-              />
-              <button type="button" onClick={() => setEditMode(false)}>
-                Save
-              </button>
-            </div>
-          )}
-        />
-      ) : (
-        <div>
-          <input
-            type="checkbox"
-            checked={task.isComplete}
-            onChange={() => dispatch({ ...task, type: 'complete' })}
-          />
-          <input
-            type="checkbox"
-            checked={task.isDeferred}
-            onChange={() => dispatch({ ...task, type: 'defer' })}
-          />
-          <input
-            type="checkbox"
-            checked={task.isCanceled}
-            onChange={() => dispatch({ ...task, type: 'cancel' })}
-          />
-          <span>{task.label}</span>
-          <button type="button" onClick={() => setEditMode(true)}>
-            Edit
-          </button>
-        </div>
-      )}
-    </div>
-  );
-};
+const Task = ({ task, dispatch }) => renderTask(dispatch, task);
 
 Task.propTypes = {
   task: PropTypes.shape({ label: PropTypes.string }).isRequired,
